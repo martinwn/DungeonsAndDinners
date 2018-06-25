cea84fed4746b877c31ba30a98d045a69d5e6892
unknown <martinwn@vcu.edu>
2018-06-24 22:34:25 -0400

    Fixed Price Filtering Issue, Added UserRAting

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -162,29 +162,38 @@ $("#findMeAPlace").on("click", function() {
 
     $('#favorite').removeClass('favorited');
 
-
     function convertRange (range) {
-       if (range === "A few minutes away") {
-           convertedRange = 3000;
-       } else if (range === "A short drive") {
-           convertedRange = 8000;
-       } else if (range === "Somewhere in town") {
-           convertedRange = 16000;
-       } else if (range === "A half hour or more") {
-           convertedRange = 80000;
-       };
+
+        switch (range) {
+
+            case "One Mile":
+                convertedRange = 1610; console.log(convertedRange);
+                break;
+
+            case "Three Miles":
+                convertedRange = 4828;
+                break;
+
+            case "Ten Miles":
+                convertedRange = 16093;
+                break;
+             
+            case "Fifty Miles":
+                convertedRange = 80467;
+                break;
+
+        };
+
     };
 
     convertRange (range); 
 
     function convertCuisine (cuisine) {
-        
-        console.log(cuisine);
 
         switch (cuisine) {
 
             case "Italian": 
-            convertCuisine = 55;
+            convertedCuisine = 55;
             $("#currentCuisine").text(cuisine);
             break;  
 
@@ -302,8 +311,7 @@ $("#findMeAPlace").on("click", function() {
     
     };
 
-
-    convertCuisine (cuisine); console.log (convertedCuisine);
+    convertCuisine (cuisine);
 
     if (convertedRange === 0 || price === "") {
         
@@ -313,191 +321,237 @@ $("#findMeAPlace").on("click", function() {
 
         $("#currentCuisine").text("");
 
+        $("#userRating").text("");
+
     } else {
 
-    var queryGoogleUrl = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBClQb1B-kxEPNM2zmAfCB2OcwWXawrHEw";
-    
-    $.ajax ({
-        url: queryGoogleUrl, 
-        method: 'POST'
-    }).then(function(response) {
+        var queryGoogleUrl = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBClQb1B-kxEPNM2zmAfCB2OcwWXawrHEw";
         
-        latitude = response.location.lat;
-        longitude = response.location.lng;
-
-        var queryZomatoUrl = "https://developers.zomato.com/api/v2.1/search?lat=" + latitude + "&lon=" + longitude + "&radius=" + convertedRange + "&cuisines=" + convertedCuisine;
-
         $.ajax ({
-            url: queryZomatoUrl,
-            method: "GET",
-            beforeSend: function(xhr){xhr.setRequestHeader("user-key", "86cbec6f776752992f95624705a3b128");}
-        }) .then(function(response) {
+            url: queryGoogleUrl, 
+            method: 'POST'
+        }).then(function(response) {
+            
+            latitude = response.location.lat;
+            longitude = response.location.lng;
 
-            matchingRestaurants.length = 0;
-                
-                for (i=0; i < response.restaurants.length; i++) {
-                
-                    if (price === "Cheap" && response.restaurants[i].restaurant.average_cost_for_two < 31) {
+            var queryZomatoUrl = "https://developers.zomato.com/api/v2.1/search?lat=" + latitude + "&lon=" + longitude + "&radius=" + convertedRange + "&cuisines=" + convertedCuisine;
+
+            $.ajax ({
+                url: queryZomatoUrl,
+                method: "GET",
+                beforeSend: function(xhr){xhr.setRequestHeader("user-key", "86cbec6f776752992f95624705a3b128");}
+            }).then(function(response) {
+
+                // Filters Ajax Results To Comply With Price Input 
+
+                matchingRestaurants.length = 0;
+                    
+                    for (i=0; i < response.restaurants.length; i++) {
+                    
+                        if (price === "Cheap" && response.restaurants[i].restaurant.average_cost_for_two < 21) {
+                            
+                            matchingRestaurants.push(response.restaurants[i]);  
+
+                        } else if (price === "Moderate" && response.restaurants[i].restaurant.average_cost_for_two > 20 && response.restaurants[i].restaurant.average_cost_for_two < 51) {
+
+                            matchingRestaurants.push(response.restaurants[i]);  
+
+                        } else if (price === "A Good Time" && response.restaurants[i].restaurant.average_cost_for_two > 50 && response.restaurants[i].restaurant.average_cost_for_two < 91) {
+
+                            matchingRestaurants.push(response.restaurants[i]);  
+
+                        } else if (price === "A REALLY Good Time" && response.restaurants[i].restaurant.average_cost_for_two > 90) {
+
+                            matchingRestaurants.push(response.restaurants[i]); 
+
+                        };
+
+                    };
+
+                // Randomly Sets Three Restaurant Objects From matchingRestaurants In A New Array
+
+                function pickThreeLocations () {
+
+                    threeRestaurantPicks.length = 0;
+
+                    if (matchingRestaurants.length > 2) {
+
+                        for (i=0; threeRestaurantPicks.length < 3;) {
+
+                            var thingy = matchingRestaurants[Math.floor(Math.random() * matchingRestaurants.length)];
+
+                            if (threeRestaurantPicks.indexOf(thingy) === -1) {
+
+                                $("#noResults").hide();
+
+                                threeRestaurantPicks.push(thingy); 
+
+                            }; 
+
+                        };
+
+                    } else if (matchingRestaurants.length === 2) {
+
+                        $("#noResults").hide();
+
+                        threeRestaurantPicks.push(matchingRestaurants[0]);
+
+                        threeRestaurantPicks.push(matchingRestaurants[1]);
+
+                    } else if (matchingRestaurants.length === 1) {
+
+                        $("#noResults").hide();
                         
-                        matchingRestaurants.push(response.restaurants[i]);  
+                        threeRestaurantPicks.push(matchingRestaurants[0]);
 
-                    } else if (price === "Moderate" && response.restaurants[i].restaurant.average_cost_for_two > 30 && response.restaurants[i].restaurant.average_cost_for_two < 61) {
+                    } else {
 
-                        matchingRestaurants.push(response.restaurants[i]);  
+                        $("#noResultsBox").text("No Results found for that Input!");
 
-                    } else if (price === "A Good Time" && response.restaurants[i].restaurant.average_cost_for_two > 60 && response.restaurants[i].restaurant.average_cost_for_two < 101) {
+                        $("#noResults").show();
 
-                        matchingRestaurants.push(response.restaurants[i]);  
+                        console.log("No results!")
 
-                    } else if (price === "A REALLY Good Time" && response.restaurants[i].restaurant.average_cost_for_two > 100) {
-
-                        matchingRestaurants.push(response.restaurants[i]); 
                     };
-
+                    
                 };
 
-            function pickThreeLocations () {
+                pickThreeLocations ();    
 
-                threeRestaurantPicks.length = 0;
+                // Randomly Sets One Restaurant Obect From threeRestaurantPicks To A New Array
+                
+                if (threeRestaurantPicks.length > 0) {
 
-                if (matchingRestaurants.length > 2) {
+                    function pickOneLocation () {
 
-                    for (i=0; threeRestaurantPicks.length < 3;) {
+                        oneRestaurantPick.length = 0;
 
-                        var thingy = matchingRestaurants[Math.floor(Math.random() * matchingRestaurants.length)];
+                        var thingy = threeRestaurantPicks[Math.floor(Math.random() * pickThreeLocations.length)];
+        
+                        oneRestaurantPick.push(thingy);
+        
+                    };
+        
+                };
 
-                        if (threeRestaurantPicks.indexOf(thingy) === -1) {
+                pickOneLocation (); console.log(oneRestaurantPick);
 
-                            $("#noResults").hide();
+                // Writes Restaurant Information From OnRestaurantPick Array Object To HTML
 
-                            threeRestaurantPicks.push(thingy); 
+                function writeRestaurantToCard (oneRestaurantPick) {
 
-                        }; 
+                    $("#mainRestaurantName").text(oneRestaurantPick[0].restaurant.name);
+
+                    $("#mainAddress").text(oneRestaurantPick[0].restaurant.location.address);
+
+                    $("#mainCuisineType").text(oneRestaurantPick[0].restaurant.cuisines);
+
+                    if (price === "Cheap") {
+                        
+                        $("#mainPriceResult").text("$");
+                        $("#favorite").attr("dataPrice", "$");
+
+
+                    } else if (price === "Moderate") {
+                        
+                        $("#mainPriceResult").text("$$");
+                        $("#favorite").attr("dataPrice", "$$");
+
+                    } else if (price === "A Good Time") {
+                        
+                        $("#mainPriceResult").text("$$$");
+                        $("#favorite").attr("dataPrice", "$$$");
+
+
+                    } else if (price === "A REALLY Good Time") {
+                        
+                        $("#mainPriceResult").text("$$$$");
+                        $("#favorite").attr("dataPrice", "$$$$");
+
+                    }; 
+
+                    // Adding Link To Menu
+
+                    $("#linkMenu").attr("href", oneRestaurantPick[0].restaurant.menu_url);
+
+                    // Adding Functionality To Reroll Button
+
+                    $(document).on("click", "#reroll", function() {
+
+                        if (threeRestaurantPicks.length === 1) {
+
+                            $("#noResultsBox").text("Only 1 Result For That Input!");
+
+                            $("#noResults").show();
+
+                        } else {
+
+                            pickThreeLocations ();
+                            pickOneLocation ();
+                            writeRestaurantToCard (oneRestaurantPick);
+                            $('#favorite').removeClass('favorited');
+
+                        };
+                        
+                    });
+
+                    // Writing Image To HTML If Available
+
+                    if (oneRestaurantPick[0].restaurant.featured_image !== "") {
+
+                        $("#mainVenuePic").attr("src", oneRestaurantPick[0].restaurant.featured_image);
+
+                        $("#mainVenuePic").show();
+
+                    } else {
+
+                        $("#mainVenuePic").hide();
 
                     };
 
-                } else if (matchingRestaurants.length === 2) {
+                    // Adding Phone Number If Available
 
-                    $("#noResults").hide();
+                    $("#phoneNumber").attr("href", "tel:+" + oneRestaurantPick[0].restaurant.phone_numbers)
 
-                    threeRestaurantPicks.push(matchingRestaurants[0]);
+                    $("#phoneNumber").text(oneRestaurantPick[0].restaurant.phone_numbers);
 
-                    threeRestaurantPicks.push(matchingRestaurants[1]);
+                    // Adding Functionality To Directions Button With Google Maps
 
-                } else if (matchingRestaurants.length === 1) {
+                    var googleUrl = "https://www.google.com/maps/search/?api=1&query=" + oneRestaurantPick[0].restaurant.location.latitude + "," + oneRestaurantPick[0].restaurant.location.longitude;
 
-                    $("#noResults").hide();
-                    
-                    threeRestaurantPicks.push(matchingRestaurants[0]);
+                    $("#linkDirections").attr("href", googleUrl);
 
-                } else {
+                    // Writing User Rating To HTML
 
-                    $("#noResultsBox").text("No Results found for that Input!");
+                    $("#userRating").text(oneRestaurantPick[0].restaurant.user_rating.aggregate_rating);
 
-                    $("#noResults").show();
+                    $("#userRating").css("backgroundColor", "#" +  oneRestaurantPick[0].restaurant.user_rating.rating_color);
+                
+                    //adding all of these for favorites use
 
-                    console.log("No results!")
+                    $("#favorite").attr("dataValue", oneRestaurantPick[0].restaurant.id);
+                    $("#favorite").attr("dataName", oneRestaurantPick[0].restaurant.name);
+                    $("#favorite").attr("dataAddress", oneRestaurantPick[0].restaurant.location.address);
+                    $("#favorite").attr("dataCuisine", oneRestaurantPick[0].restaurant.cuisines);
+                    $("#favorite").attr("dataMenu", oneRestaurantPick[0].restaurant.menu_url);
 
                 };
                 
-            };
-
-            pickThreeLocations ();    
+                writeRestaurantToCard (oneRestaurantPick);
+                
+            });
             
-            if (threeRestaurantPicks.length > 0) {
+        }); 
 
-                function pickOneLocation () {
-
-                    oneRestaurantPick.length = 0;
-
-                    var thingy = threeRestaurantPicks[Math.floor(Math.random() * pickThreeLocations.length)];
-    
-                    oneRestaurantPick.push(thingy);
-    
-                };
-    
-            };
-
-            pickOneLocation (); console.log(oneRestaurantPick);
-
-            function writeRestaurantToCard (oneRestaurantPick) {
-
-                $("#mainRestaurantName").text(oneRestaurantPick[0].restaurant.name);
-
-                $("#mainAddress").text(oneRestaurantPick[0].restaurant.location.address);
-
-                $("#mainCuisineType").text(oneRestaurantPick[0].restaurant.cuisines);
-
-                if (price === "Cheap") {
-                    
-                    $("#mainPriceResult").text("$");
-                    $("#favorite").attr("dataPrice", "$");
-
-
-                } else if (price === "Moderate") {
-                    
-                    $("#mainPriceResult").text("$$");
-                    $("#favorite").attr("dataPrice", "$$");
-
-                } else if (price === "A Good Time") {
-                    
-                    $("#mainPriceResult").text("$$$");
-                    $("#favorite").attr("dataPrice", "$$$");
-
-
-                } else if (price === "A REALLY Good Time") {
-                    
-                    $("#mainPriceResult").text("$$$$");
-                    $("#favorite").attr("dataPrice", "$$$$");
-
-                } 
-
-                // $("#mainVenuePic").attr("src", oneRestaurantPick[0].restaurant.featured_image);
-
-                // $("#phoneNumber").attr("href", "tel:+" + oneRestaurantPick[0].restaurant.phone_numbers)
-
-                $("#linkMenu").attr("href", oneRestaurantPick[0].restaurant.menu_url);
-
-                $("#favorite").attr("dataValue", oneRestaurantPick[0].restaurant.id);
-
-                $(document).on("click", "#reroll", function() {
-
-
-                    pickThreeLocations ();
-                    pickOneLocation ();
-                    writeRestaurantToCard (oneRestaurantPick);
-                    $('#favorite').removeClass('favorited');
-
-                    
-                });
-
-                var googleUrl = "https://www.google.com/maps/search/?api=1&query=" + oneRestaurantPick[0].restaurant.location.latitude + "," + oneRestaurantPick[0].restaurant.location.longitude;
-
-                $("#linkDirections").attr("href", googleUrl);
-              
-                //adding all of these for favorites use
-
-                $("#favorite").attr("dataValue", oneRestaurantPick[0].restaurant.id);
-                $("#favorite").attr("dataName", oneRestaurantPick[0].restaurant.name);
-                $("#favorite").attr("dataAddress", oneRestaurantPick[0].restaurant.location.address);
-                $("#favorite").attr("dataCuisine", oneRestaurantPick[0].restaurant.cuisines);
-                $("#favorite").attr("dataMenu", oneRestaurantPick[0].restaurant.menu_url);
-
-            };
-            
-            writeRestaurantToCard (oneRestaurantPick);
-            
-        });
-        
-    }); 
-
-};
+    };
 
 });
 
 //building favorites code
 
 $(document).on("click", "#favorite", function() {
+
     if (userLoggedIn === true) {
 
         if (!$(this).hasClass("favorited")) {
