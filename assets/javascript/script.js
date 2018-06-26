c1b5acc28616c08b4c3bf1bf3fa9a569b9568d2e
WMABABB <wm.andrew.babb@gmail.com>
2018-06-26 14:06:05 -0400

    favredraw

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -499,11 +499,9 @@ $(document).on("click", "#favorite", function() {
             globalUID.update({
                 favoritesListDB: favoritesLocal,
             });
-
-            console.log(favorites)
         
             $("#favoritesBox").empty();
-            
+
             displayFavorites();
             console.log('seeing display favorites from inside remove favorites'); //this should only go off if favoritesShowing === true
             //it's to redraw displayed favorites to keep the displayed dataIndexes from being off
